13a87aa5b64af8d084b91ebb3add1a239fc86cca
Lin Thit Htoo <lth1212001@gmail.com>
2025-01-17 11:01:02 +0630

    -

---
+++ LongPolling.HttpClient/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        try
+        {
+            HttpClient client = new()
+            {
+                BaseAddress = new Uri("http://localhost:5005")
+            };
+
+            Console.WriteLine("Calling API...");
+            HttpResponseMessage response = await client.GetAsync("/efficient");
+            response.EnsureSuccessStatusCode();
+            string jsonStr = await response.Content.ReadAsStringAsync();
+
+            Console.WriteLine($"Response: {jsonStr}");
+        }
+        catch (Exception ex)
+        {
+            throw;
+        }
+    }
+}
